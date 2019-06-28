6569d71035da69701eb1f77beb6e47990f03975f
RUTONG <lucien33@live.com>
2019-06-28 16:25:12 +0200

    final

--- probleme.php
+++ probleme.php
@@ -4,7 +4,7 @@ MDP oublié
 
 Compte Client => valider la commande => pages commandes / page home->name / page change password
 
-Compte Admin
+Compte Admin => Dashboard / Commandes / Facturation
 
 Super Admin ==> Gestion toutes les données et Gestion des roles de tous les utilisateurs
 
